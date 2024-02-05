a2ca26c180c05b36c5865c16ed3f0c248efb99a6
Sam Usherenko <SamUsherenko@gmail.com>
2024-02-05 01:10:40 -0500

    Introduced example db for testing.

--- Fast Api Python/main.py
+++ Fast Api Python/main.py
@@ -2,7 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+dataBase = {
+    1: {
+        "name": "John Doe",
+        "address": "123 Main St",
+        "email": "johndoe@example.com"
+    },
+    2: {
+        "name": "Jane Doe",
+        "address": "456 Side St",
+        "email": "janedoe@example.com"
+    },
+    3: {
+        "name": "John Smith",
+        "address": "789 Back St",
+        "email": "SmithJohn@hotexample.com"
+    }
+}
 
-@app.get("/")
-def home():
-    return {"message": "Hello, World!"}
+
+@app.get("/get-user/{user_id}")
+def get_user(user_id: int):
+
+    return dataBase[user_id]
