c104f8dba853c6f8885892a0c8da78e9675cdff2
Sam Usherenko <SamUsherenko@gmail.com>
2024-02-06 14:36:39 -0500

    Updated for GET, POST and PUT method calls. Included error trapping for invalid input.

--- Fast API Python/main.py
+++ Fast API Python/main.py
@@ -1,32 +1,61 @@
 from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
-database = {
-    1: {
-        "name": "John",
-        "age": 31,
-        "city": "New York"
-    },
-    2: {
-        "name": "John",
-        "age": 31,
-        "city": "New York"
-    },
-    3: {
-        "name": "John",
-        "age": 31,
-        "city": "New York"
-    },
-}
 
 
-@app.get("/get-users/{user_id}")
+class User(BaseModel):
+    name: str
+    age: int
+    city: str
+    state: Optional[str] = None
+
+
+class UpdateUser(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+
+
+database = {}
+
+
+@app.get("/get-user/{user_id}")
 def get_id(user_id: int = Path(None, description="The ID of the user you'd like to view", gt=0, lt=2)):
     return database[user_id]
 
 
-@app.get("/get-by-name")
-def get_user(name: str):
+@app.get("/get-by-name/{user_id}")
+def get_user(*, user_id: int, name: Optional[str] = None, test: int):
     for user_id in database:
-        if database[user_id]["name"] == name:
+        if database[user_id].name == name:
             return database[user_id]
     return {"Error": "User not found"}
+
+
+@app.post("/create-user/{user_id}")
+def create_user(user_id: int, user: User):
+    if user_id in database:
+        return {"Error": "User exists"}
+
+    database[user_id] = {"name": user.name, "age": user.age,
+                         "city": user.city, "state": user.state}
+    return database[user_id]
+
+
+@app.put("/update-user/{user_id}")
+def update_user(user_id: int, user: User):
+    if user_id not in database:
+        return {"Error": "User does not exist"}
+
+    if user.name != None:
+        database[user_id]["name"] = user.name
+
+    if user.age != None:
+        database[user_id]["age"] = user.age
+
+    if user.city != None:
+        database[user_id]["city"] = user.city
+
+    return database[user_id]
